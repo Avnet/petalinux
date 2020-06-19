3af2b017cc9068e592463dd39e1617fb2579fa73
tnizan-witekio <tnizan@witekio.com>
2020-06-19 15:43:37 -0700

    uz3eg_iocc: add pmic-prog application
    pmic-prog application is used to program and verify PMICs on the board
    
    Currently supported PMICs: IRPS5401, IR3806x

---
+++ configs/meta-user/uz3eg_iocc/recipes-apps/pmic-prog/files/ir3806x.h
@@ -0,0 +1,60 @@
+// ----------------------------------------------------------------------------
+//
+//        ** **        **          **  ****      **  **********  **********
+//       **   **        **        **   ** **     **  **              **
+//      **     **        **      **    **  **    **  **              **
+//     **       **        **    **     **   **   **  *********       **
+//    **         **        **  **      **    **  **  **              **
+//   **           **        ****       **     ** **  **              **
+//  **  .........  **        **        **      ****  **********      **
+//     ...........
+//                                     Reach Further
+//
+// ----------------------------------------------------------------------------
+//
+//  This design is the property of Avnet.  Publication of this
+//  design is not authorized without written consent from Avnet.
+//
+//  Please direct any questions to the Zed community support forum:
+//     http://zedboard.org/forums/zed-english-forum
+//
+//  Product information is available at:
+//     http://zedboard.org/
+//
+//  Disclaimer:
+//     Avnet, Inc. makes no warranty for the use of this code or design.
+//     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+//     any errors, which may appear in this code, nor does it make a commitment
+//     to update the information contained herein. Avnet, Inc specifically
+//     disclaims any implied warranties of fitness for a particular purpose.
+//                      Copyright(c) 2018 Avnet, Inc.
+//                              All rights reserved.
+//
+// ----------------------------------------------------------------------------
+//
+//  Create Date:         Jun 16, 2020
+//  Design Name:         PMIC PROGRAMMER
+//  Module Name:         ir3806x.h
+//  Project Name:        PMIC PROGRAMMER
+//
+//  Description:         Application to program Infineon PMICs
+//
+//  Dependencies:
+//
+//  Revision:            Jun 16, 2020: 1.0 Initial version
+//
+// ----------------------------------------------------------------------------
+
+#ifndef IR3806x_H_
+#define IR3806x_H_
+
+#include <stdint-gcc.h>
+#include <stdbool.h>
+
+bool ir3806x_detect ( uint8_t *version );
+bool ir3806x_read_byte ( uint16_t address, uint8_t *res );
+bool ir3806x_write_byte ( uint16_t address, uint8_t value );
+bool ir3806x_read_all_registers ( const char *output_filename, const char *compare_filename );
+bool ir3806x_program ( bool dry_run, const char *input_filename );
+
+#endif /* IR3806x_H_ */
