b628cd02b11307b97a8556130875c3f7c8e5f71b
Thomas Nizan <tnizan@witekio.com>
2020-08-19 16:46:45 -0700

    uz7ev-evcc: add 2020 initial support

---
+++ configs/project/config.board.uz7ev_evcc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+KCONFIG_EDIT=${PETALINUX}/tools/common/petalinux/utils/petalinux-kconfig-edit
+CONFIG_FILE=project-spec/configs/config
+
+${KCONFIG_EDIT} -c ${CONFIG_FILE} -o CONFIG_SUBSYSTEM_HOSTNAME -v "\"uz7ev-evcc-2020-1\""
+${KCONFIG_EDIT} -c ${CONFIG_FILE} -o CONFIG_YOCTO_MACHINE_NAME -v "\"uz7ev-evcc\""
+${KCONFIG_EDIT} -c ${CONFIG_FILE} -o CONFIG_USER_LAYER_0 -v "\"\${PROOT}/project-spec/meta-avnet\""
