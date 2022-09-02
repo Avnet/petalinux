519231ad00248b67190b6deb220f72ece41feefd
Bruce Thomson <bthomson@witekio.com>
2022-09-02 12:46:46 +0100

    Merged in zub1cg changes from previous 2021.2 branches

---
+++ scripts/boot/zub1cg/boot_qspi_INITRD_MINIMAL.sh
@@ -0,0 +1,44 @@
+# ----------------------------------------------------------------------------
+#
+#        ** **        **          **  ****      **  **********  ********** ®
+#       **   **        **        **   ** **     **  **              **
+#      **     **        **      **    **  **    **  **              **
+#     **       **        **    **     **   **   **  *********       **
+#    **         **        **  **      **    **  **  **              **
+#   **           **        ****       **     ** **  **              **
+#  **  .........  **        **        **      ****  **********      **
+#     ...........
+#                                     Reach Further™
+#
+# ----------------------------------------------------------------------------
+#
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+#
+#  Please direct any questions to the PicoZed community support forum:
+#     http://avnet.me/zub1cg_forum
+#
+#  Product information is available at:
+#     http://avnet.me/zub1cg
+#
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2021 Avnet, Inc.
+#                              All rights reserved.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# This script will generate a BOOT.BIN file and program the qspi
+# This BOOT.BIN file will contain uboot, a kernel with INITRD and a boot.scr
+
+# Stop the script whenever we had an error (non-zero returning function)
+set -e
+
+petalinux-package --boot --fsbl ./images/linux/zynqmp_fsbl.elf --fpga ./images/linux/system.bit --uboot --kernel ./image_INITRD_MINIMAL.ub -o BOOT_LINUX_QSPI.BIN --force --boot-device flash --add ./images/linux/avnet-boot/avnet_qspi.scr --offset 0x1FC0000
+
+program_flash -f ./BOOT_LINUX_QSPI.BIN -offset 0 -flash_type qspi-x4-single -fsbl ./images/linux/zynqmp_fsbl.elf
---
+++ scripts/boot/zub1cg/create_dhcp_boot_scr.sh
@@ -0,0 +1,74 @@
+# ----------------------------------------------------------------------------
+#
+#        ** **        **          **  ****      **  **********  ********** ®
+#       **   **        **        **   ** **     **  **              **
+#      **     **        **      **    **  **    **  **              **
+#     **       **        **    **     **   **   **  *********       **
+#    **         **        **  **      **    **  **  **              **
+#   **           **        ****       **     ** **  **              **
+#  **  .........  **        **        **      ****  **********      **
+#     ...........
+#                                     Reach Further™
+#
+# ----------------------------------------------------------------------------
+#
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+#
+#  Please direct any questions to the UltraZed community support forum:
+#     http://avnet.me/uzegforum and http://avnet.me/uzevforum
+#
+#  Product information is available at:
+#     http://avnet.me/ultrazed-eg and http://avnet.me/ultrazed-ev
+#
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2021 Avnet, Inc.
+#                              All rights reserved.
+#
+# ----------------------------------------------------------------------------
+#!/bin/bash
+
+# This script will generate a u-boot boot script
+# This $BOOT_SCR_NAME.scr will get an ip address from dhcp,
+#    set the tftp server ip (given by argument to this script), and start booting the board with tftp
+
+# Stop the script whenever we had an error (non-zero returning function)
+set -e
+
+BOOT_SCR_NAME="avnet_jtag_tftp_dhcp"
+
+if ! mkimage -V > /dev/null 2>&1
+then 
+    echo -e "  mkimage not installed \n  try: sudo apt install u-boot-tools\n"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ];
+then
+    echo -e "  \"create_dhcp_boot_scr.sh\" requires exactly 1 argument.\n  Usage:  sh create_dhcp_boot_scr.sh TFTP_SERVER_IP"
+    exit 1
+fi
+
+SERVER_IP=$1
+echo -e "  Will use $SERVER_IP as the TFTP server IP"
+
+cat <<EOT > $BOOT_SCR_NAME.scr.txt
+################
+# This is a boot script for U-Boot
+#
+################
+setenv autostart yes
+setenv autoload no
+dhcp
+setenv serverip $SERVER_IP
+tftpboot 0x04000000 image.ub
+EOT
+
+mkimage -A arm -T script -C none -n "Ubuntu boot script" -d $BOOT_SCR_NAME.scr.txt $BOOT_SCR_NAME.scr
+
+rm $BOOT_SCR_NAME.scr.txt
